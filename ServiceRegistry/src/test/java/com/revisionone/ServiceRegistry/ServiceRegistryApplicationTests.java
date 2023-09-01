fe821a5c5df4e43fc14cb07a88e0a37eb8df4a73
vikas kambhampati <vikaskambhampati@gmail.com>
2023-09-01 12:14:50 -0700

    Added Eureka Service Registry

---
+++ ServiceRegistry/src/test/java/com/revisionone/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revisionone.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
